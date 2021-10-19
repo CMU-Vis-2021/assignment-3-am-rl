f9d30c8d29fb9e9d5d7317752167d4f5a37f6558
rob-lambeth <rlambeth3@gmail.com>
2021-10-19 09:25:06 -0400

    v1 tweaks - still getting errors

--- v1.js
+++ v1.js
@@ -1,12 +1,12 @@
-export let v1_spec = {  
+export let v1_spec = {
   $schema: "https://vega.github.io/schema/vega-lite/v5.json",
   data: { name: "table" },
-  mark: "bar",
+  mark: { type: "bar" },
   params: [
     {
       name: "brush",
-      select: { type: "interval", encodings: ["x"] }
-    }
+      select: { type: "interval", encodings: ["x"] },
+    },
   ],
   encoding: {
     y: {
@@ -16,12 +16,10 @@ export let v1_spec = {
       title: null,
       axis: { labelLimit: 300 },
     },
-    x: { field: "count", type: "quantitative", title: "Inmate count" },
-    color: {
-      condition: {
-        selection: "pts",
-      },
-      value: "grey",
+    x: {
+      field: "count",
+      type: "quantitative",
+      title: "Inmate count",
     },
   },
-})
+}
